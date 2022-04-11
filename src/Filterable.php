17589db8c8f0b75fcf783fa1d0d30a8843f9490a
Scott Martens <scott@chargenation.io>
2022-04-11 15:14:16 +1000

    Define return type

--- src/Filterable.php
+++ src/Filterable.php
@@ -37,7 +37,7 @@ trait Filterable
      *
      * @return array
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): array
     {
         return array_merge(parent::jsonSerialize(), [
             'filters' => collect($this->filters() ?? [])->map(function ($filter) {
